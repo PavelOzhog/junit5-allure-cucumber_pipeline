d344c108043ed10127edc9a0d2a8a9af9757688a
PavelOzhog <flashog@mail.ru>
2021-08-19 22:48:15 +0300

    изменение файла параметров TAG

--- Jenkinsfile_library.groovy
+++ Jenkinsfile_library.groovy
@@ -1,16 +1,19 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 pipeline {
-    agent {node { label 'linux'}}
+    agent any
+    parameters{
+        string(name='TAG', defaultValue:@smoke , description= 'тег для запуска')
+    }
     stages {
         stage('Build') {
             steps {
-                sh "${mvn} clean install"
+                sh "${mvn} clean verify"
             }
         }
         stage('Run Tests') {
             steps {
-                sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+                sh "${mvn} test -Dcucumber.filter.tags=\"${params.TAG}\""
             }
         }
         stage('Allure Report Generation') {
