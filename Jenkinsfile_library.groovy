e92ce7e083575f36c0f360404625cd21f3886d1c
PavelOzhog <flashog@mail.ru>
2021-08-19 23:06:46 +0300

    изменение файла параметров TAG

--- Jenkinsfile_library.groovy
+++ Jenkinsfile_library.groovy
@@ -1,13 +1,8 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 pipeline {
-    agent any
-    parameters {
-        agent { label 'linux' }
-        parameters {
-            string(name: 'TAG', defaultValue: '@smoke', description: 'тег для запуска')
-        }
-        stages {
+        agent {node { label 'linux'}}
+           stages {
             stage('Build') {
                 steps {
                     sh "${mvn} clean verify"
@@ -28,4 +23,3 @@ pipeline {
 
         }
     }
-}
